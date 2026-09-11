aff36f0d08d836b909a865b02b91300b36dd03e5
luisjnr <luiscarlossantossilvajunior@gmail.com>
2026-09-11 12:17:20 -0300

    incidencia

---
+++ Matrix/incidencia.py
@@ -0,0 +1,16 @@
+matriz_incidencia = [
+	[1, 1, 0, 0],
+	[1, 0, 1, 0],
+	[0, 1, 1, 1],
+	[0, 0, 0, 1]
+]
+
+def matriz_para_incidencia(matriz, coluna):
+	if(coluna > (len(matriz[0]) - 1)):
+		return 
+	print(f"Aresta {coluna}")
+	for i, linha in enumerate(matriz):
+		if(linha[coluna] == 1):
+			print(f"Vértice {i}: [{linha[coluna]}]")
+
+matriz_para_incidencia(matriz_incidencia, 2)
